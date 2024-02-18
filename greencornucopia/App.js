01003a777884c4e6ef68a9165588f53672075480
sidtea123 <sidtea123@gmail.com>
2024-02-17 20:36:17 -0500

    better gutting
    I forgor to delete some references

--- greencornucopia/App.js
+++ greencornucopia/App.js
@@ -3,8 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import AuthPage from './OtherPages/AuthPage';
-import LoginPage from './OtherPages/AuthPage';
-import SignupPage from './OtherPages/SignupPage';
 import DashboardPage from './OtherPages/DashboardPage';
 
 const Stack = createStackNavigator();
