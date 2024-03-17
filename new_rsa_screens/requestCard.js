13185b540c0fd12cb62ca6e2c552b0962f78843b
sidtea123 <sidtea123@gmail.com>
2024-03-17 18:58:30 -0400

    idk, cards dopn't work

--- new_rsa_screens/requestCard.js
+++ new_rsa_screens/requestCard.js
@@ -5,87 +5,109 @@ import {
   ImageBackground,
   SafeAreaView,
   ScrollView,
+  SectionList,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
-const RequestCardView = () => {
+
+const RequestCardView = (data) => {
   return (
-    <SafeAreaView>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-        }}>
-        {/*upper bar collection*/}
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}>
-          {/*pfp*/}
-          <ImageBackground
-            source={require('./assets/images/de8116f3-5eab-4da6-a85d-0ea13fdd6186.png')}
-          />
-          {/* card title */}
-          <Text
+    <View
+      style={{
+        padding: 10
+      }}>
+      {[data].map((request) => {
+        return (
+          <View
             style={{
-              display: 'flex',
-              height: '100%',
-              justifyContent: 'flex-start',
-              alignItems: 'flex-start',
-              fontFamily: 'Poppins',
-              fontSize: 20,
-              fontWeight: '400',
-              lineHeight: 30,
-              color: '#030303',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              textAlign: 'left',
-              zIndex: 19,
+              borderRadius: 10,
+              backgroundColor: '#b1eba4',
+              alignContent: 'stretch',
+              justifyContent: 'stretch',
+              padding: 5,
+              alignItems: 'stretch',
             }}
-            numberOfLines={1}>
-            Fresh Fruit Needed!
-          </Text>
-        </View>
-        {/* rest of card */}
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'column',
-          }}>
-          <Text
-            style={{
-              display: 'flex',
-              height: '100%',
-              justifyContent: 'flex-start',
-              alignItems: 'flex-start',
-              fontFamily: 'Poppins',
-              fontSize: 16,
-              fontWeight: '400',
-              color: '#030303',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              textAlign: 'left',
-              zIndex: 20,
-            }}>
-            Apples{'\n'}Oranges{'\n'}Bananas{'\n'}
-            {'\n'}Food Bank in Charlotte
-          </Text>
-          {/* card image */}
-          <ImageBackground
-            style={{
-              position: 'absolute',
-              top: 0,
-              zIndex: 22,
+          >
+            <View style={{
+              flex: 1,
+              padding: 20,
+
+              flexDirection: 'row',
+              alignContent: 'stretch',
+              justifyContent: 'stretch'
             }}
-            source={require('./assets/images/3f0eb259e014cdc93250787756b2504869bb4195.png')}
-          />
-        </View>
-      </View>
-    </SafeAreaView>
+            >
+              <Text
+                style={{
+                  display: 'flex',
+                  height: '100%',
+                  justifyContent: 'flex-start',
+                  alignItems: 'flex-start',
+                  fontFamily: 'Poppins',
+                  fontSize: 20,
+                  fontWeight: '400',
+                  lineHeight: 30,
+                  color: '#030303',
+                  flex: 2,
+                  top: 0,
+                  left: 0,
+                  textAlign: 'center',
+                  zIndex: 19,
+                }}
+                numberOfLines={1}>
+                Fresh Fruit Needed!
+              </Text>
+              <ImageBackground
+                style={{
+                  width: 11.813,
+                  height: 4.126,
+                  marginTop: 0,
+                  marginRight: 0,
+                  marginBottom: 0,
+                  marginLeft: 0,
+                  alignContent: 'center',
+                  justifyContent: 'space-between',
+                }}
+                source={require('./assets/images/1bda9b54-ce26-4768-908e-2be87be9e56d.png')}
+              />
+            </View>
+            <View
+              style={{
+                height: 150,
+                position: 'relative',
+                zIndex: 22,
+                padding: 20,
+                flex: 1,
+              }}>
+              <Text
+                style={{
+                  display: 'flex',
+                  width: 252,
+                  height: '100%',
+                  justifyContent: 'flex-start',
+                  alignItems: 'flex-start',
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                  fontWeight: '400',
+                  lineHeight: 24,
+                  color: '#030303',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  textAlign: 'left',
+                  zIndex: 20,
+                }}>
+                Apples{'\n'}Oranges{'\n'}Bananas{'\n'}
+                {'\n'}Food Bank in Charlotte
+              </Text>
+            </View>
+          </View>
+        );
+      }
+      )
+      };
+    </View>
   );
-};
+}
+
 export default RequestCardView;
