897c8ef2e80e2d65142e17fdd4c930c9b89157c6
Prbhuv N <prbhuvnigam@gmail.com>
2024-03-17 15:29:36 -0400

    Update index.js

--- new_rsa_screens/index.js
+++ new_rsa_screens/index.js
@@ -1,6 +1,6 @@
 import { AppRegistry, Platform } from 'react-native';
 //import App from './pages/donatorPages/myDonations';
-import App from './pages/MainNavigation.js';
+import App from './pages/authPages/donorLogin.js';
 
 AppRegistry.registerComponent('main', () => App);
 
